4637fae6c8c507dfed0526edc901c5e697fe5869
qusai <qusai@wso2.com>
2019-08-25 00:28:51 +0530

    added voice

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var firebaseRef = firebase.database().ref();
 let tokenData = {};
 let tokenProperties = [];
 let displayData = "";
+let synth = window.speechSynthesis;
+let voices = synth.getVoices();
 
 firebaseRef.on("value", function (snapshot) {
     counters = snapshot.toJSON().counters;
@@ -15,6 +17,7 @@ firebaseRef.on("value", function (snapshot) {
             if (token && token.pending === true && token.accepted === false) {
                 displayData += "<li><ul class='p-3 bg-warning text-white'>";
                 displayData += "<li class='tokenStatus'>" + counters[counterDetails[i]].name + " Counter <strong class='counterNum'>" + counters[counterDetails[i]].number + "</strong> - Token <strong class='tokenNum blink'>" + counters[counterDetails[i]].token + "</strong></li>";
+                playVoice("Token number " + counters[counterDetails[i]].token + ", please proceed to " + counters[counterDetails[i]].name + " Counter " + counters[counterDetails[i]].number);
             } else if (token && token.pending === false && token.accepted === true) {
                 displayData += "<li><ul class='p-3 bg-success text-white'>";
                 displayData += "<li class='tokenStatus'>" + counters[counterDetails[i]].name + " Counter <strong class='counterNum'>" + counters[counterDetails[i]].number + "</strong> - Token <strong class='tokenNum'>" + counters[counterDetails[i]].token + "</strong></li>";
@@ -26,11 +29,35 @@ firebaseRef.on("value", function (snapshot) {
             displayData += "</ul></li><br>";
         }
     }
+
     document.querySelector("ul.tokensList").innerHTML = displayData;
     tokenData = {};
     tokenProperties = [];
     displayData = "";
 });
+
+function playVoice(textToSpeak) {
+    populateVoices();
+    if(speechSynthesis !== undefined){
+        speechSynthesis.onvoiceschanged = populateVoices;
+    }
+    let toSpeak = new SpeechSynthesisUtterance(textToSpeak);
+    toSpeak.voice = voices[0];
+    window.speechSynthesis.speak(toSpeak);
+}
+
+function populateVoices(){
+    voices.forEach((voice)=>{
+        var listItem = document.createElement('option');
+        listItem.textContent = voice.name;
+        listItem.setAttribute('data-lang', voice.lang);
+        listItem.setAttribute('data-name', voice.name);
+    });
+}
+
+function removeDuplicates(value, index, self) {
+    return self.indexOf(value) === index;
+}
 
 // firebaseRef.on('value', function (snapshot) {
 //     if (snapshot.toJSON().tokens) {
