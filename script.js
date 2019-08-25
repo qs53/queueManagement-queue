9039db02e90414eaab53b7c2698501c2eaca647c
qusai <qusai@wso2.com>
2019-08-25 13:41:26 +0530

    added prefix

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ firebaseRef.on("value", function (snapshot) {
             token = snapshot.toJSON().tokens[tokenNo];
             if (token && token.pending === true && token.accepted === false) {
                 displayData += "<li><ul class='p-3 bg-warning text-white'>";
-                displayData += "<li class='tokenStatus'>" + counters[counterDetails[i]].name + " Counter <strong class='counterNum'>" + counters[counterDetails[i]].number + "</strong> - Token <strong class='tokenNum blink'>" + counters[counterDetails[i]].token + "</strong></li>";
+                displayData += "<li class='tokenStatus'>" + counters[counterDetails[i]].name + " Counter <strong class='counterNum'>" + counters[counterDetails[i]].number + "</strong> - Token <strong class='tokenNum blink'>" + counters[counterDetails[i]].prefix + counters[counterDetails[i]].token + "</strong></li>";
                 playVoice("Token number " + counters[counterDetails[i]].token + ", please proceed to " + counters[counterDetails[i]].name + " Counter " + counters[counterDetails[i]].number);
             } else if (token && token.pending === false && token.accepted === true) {
                 displayData += "<li><ul class='p-3 bg-success text-white'>";
-                displayData += "<li class='tokenStatus'>" + counters[counterDetails[i]].name + " Counter <strong class='counterNum'>" + counters[counterDetails[i]].number + "</strong> - Token <strong class='tokenNum'>" + counters[counterDetails[i]].token + "</strong></li>";
+                displayData += "<li class='tokenStatus'>" + counters[counterDetails[i]].name + " Counter <strong class='counterNum'>" + counters[counterDetails[i]].number + "</strong> - Token <strong class='tokenNum'>" + counters[counterDetails[i]].prefix + counters[counterDetails[i]].token + "</strong></li>";
             }
             displayData += "</ul></li><br>";
         } else {
