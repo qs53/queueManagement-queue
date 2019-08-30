77ea32ff16b2b50af8ae275dae0dcef1c5b08f3b
qusai <qusai@wso2.com>
2019-08-30 23:30:10 +0530

    side by side column

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ var firebaseRef = firebase.database().ref();
 let tokenData = {};
 let tokenProperties = [];
 let displayData = "";
-let synth = window.speechSynthesis;
-let voices = synth.getVoices();
 
 firebaseRef.on("value", function (snapshot) {
     let counters = snapshot.toJSON().counters;
@@ -21,23 +19,27 @@ firebaseRef.on("value", function (snapshot) {
 
     document.querySelector("ul.tokensList").innerHTML = "";
     for (let i = 0; i < counterDetails.length; i++) {
+        i % 2 === 0 ? displayData += "<div class='row'>" : displayData += "";
         if (counters[counterDetails[i]].token && snapshot.toJSON().tokens) {
             let tokenNo = counters[counterDetails[i]].token;
             let token = snapshot.toJSON().tokens[tokenNo];
             if (token && token.pending === true && token.accepted === false) {
-                displayData += "<li><ul class='p-3 bg-warning text-white'>";
+                displayData += "<div class='col'><li><ul class='p-3 bg-warning text-white'>";
                 displayData += "<li class='tokenStatus'>" + counters[counterDetails[i]].name + " Counter <strong class='counterNum'>" + counters[counterDetails[i]].number + "</strong> - Calling Token <strong class='tokenNum blink'>" + counters[counterDetails[i]].prefix + counters[counterDetails[i]].token + "</strong></li>";
-                // playVoice("Token number " + counters[counterDetails[i]].token + ", please proceed to " + counters[counterDetails[i]].name + " Counter " + counters[counterDetails[i]].number);
             } else if (token && token.pending === false && token.accepted === true) {
-                displayData += "<li><ul class='p-3 bg-success text-white'>";
+                displayData += "<div class='col'><li><ul class='p-3 bg-success text-white'>";
                 displayData += "<li class='tokenStatus'>" + counters[counterDetails[i]].name + " Counter <strong class='counterNum'>" + counters[counterDetails[i]].number + "</strong> - Attending To Token <strong class='tokenNum'>" + counters[counterDetails[i]].prefix + counters[counterDetails[i]].token + "</strong></li>";
             }
-            displayData += "</ul></li><br>";
+            displayData += "</ul></li></div><br>";
         } else {
-            displayData += "<li><ul class='p-3 bg-info text-white'>";
+            displayData += "<div class='col'><li><ul class='p-3 bg-info text-white'>";
             displayData += "<li class='tokenStatus'>" + counters[counterDetails[i]].name + " Counter <strong class='counterNum'>" + counters[counterDetails[i]].number + "</strong> - Token Allocation Pending</li>";
-            displayData += "</ul></li><br>";
+            displayData += "</ul></li></div><br>";
         }
+        if ((counterDetails.length % 2 === 1) && (i === counterDetails.length - 1)) {
+            displayData += "<div class='col'></div>"
+        }
+        i % 2 === 1 ? displayData += "</div>" : displayData += "";
     }
 
     document.querySelector("ul.tokensList").innerHTML = displayData;
@@ -45,30 +47,6 @@ firebaseRef.on("value", function (snapshot) {
     tokenProperties = [];
     displayData = "";
 });
-
-function playVoice(textToSpeak) {
-    populateVoices();
-    if(speechSynthesis !== undefined){
-        speechSynthesis.onvoiceschanged = populateVoices;
-    }
-    let toSpeak = new SpeechSynthesisUtterance(textToSpeak);
-    toSpeak.voice = voices[0];
-    window.speechSynthesis.speak(toSpeak);
-}
-
-function populateVoices(){
-    voices.forEach((voice)=>{
-        var listItem = document.createElement('option');
-        listItem.textContent = voice.name;
-        listItem.setAttribute('data-lang', voice.lang);
-        listItem.setAttribute('data-name', voice.name);
-    });
-}
-
-function removeDuplicates(value, index, self) {
-    return self.indexOf(value) === index;
-}
-
 
 function sortNumber(a, b) {
     return a - b;
